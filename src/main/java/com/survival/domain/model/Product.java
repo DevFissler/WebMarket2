7ae9c9732f29412088e9d84114885959c5473c0a
devfissler <oruma66@naver.com>
2022-06-14 17:37:27 +0900

    first commit

---
+++ src/main/java/com/survival/domain/model/Product.java
@@ -0,0 +1,131 @@
+package com.survival.domain.model;
+
+import java.util.Objects;
+
+//1. 무지성 private 맴버 변수
+//2. 필요하면 생성자 추가
+//3. 무지성 getter / setter
+//4. 무지성 toString()
+//5. 필요하면 equals / hashCode 재정의 (오버라이드)
+
+public class Product {
+	public static void main(String[] args) {
+		Product product = new Product("33", "33", 11);
+		System.out.println(product);
+
+		Product product2 = new Product("33", "33", 11);
+		System.out.println(product2);
+
+		// == : 주소비교, hasCode()가 같아야한다. 내용이 같더라도 다를 수 있다.
+		// equals : Object 로 받아와서 비교, 모든 클래스는 Object(타입을 가리지 않음)를 상속받는다.(extends),
+		// Object object = new Product("33", "33", 11); 도 가능하다.
+		// product.equals(product2); // true - 재정의함
+		// product.hashCode() == product2.hashCode(); //true 재정의
+		// product == product2 // false
+	}
+
+	private String id;
+	private String name;
+	private int unitprice;
+	private String description;
+	private String manufacturer;
+	private String category;
+	private long unitsInStock;
+	private String condition;
+
+	// overload - 똑같은 이름의 메소드를 인자만 다르게해서 받음 / override 재정의
+	public Product(String id, String name, int unitprice) {
+		this.id = id;
+		this.name = name;
+		this.unitprice = unitprice;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getUnitPrice() {
+		return unitprice;
+	}
+
+	public void setUnitPrice(int unitprice) {
+		this.unitprice = unitprice;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public long getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(long unitInStock) {
+		this.unitsInStock = unitInStock;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", unitprice=" + unitprice + ", description=" + description
+				+ ", manufacturer=" + manufacturer + ", category=" + category + ", unitsInStock=" + unitsInStock
+				+ ", condition=" + condition + "]";
+	}
+
+}
---
+++ src/main/webapp/footer.jsp
@@ -0,0 +1,3 @@
+<footer class="container">
+	<p>&copy; JH Market</p>
+</footer>
